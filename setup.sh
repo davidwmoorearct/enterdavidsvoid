4067faa901cfc12e90bc93f402f1c58977af9c8c
David W. Moore <dmoor231@students.kennesaw.edu>
2024-09-30 12:31:33 -0400

    fixing ohmyzsh install.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo xbps-install -Sy $(cat packages.txt)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp .xinitrc ~/
 cp .zshrc ~/
 sudo cp config.toml /etc/greetd/config.toml
