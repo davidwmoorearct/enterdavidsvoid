e8d2f7244d0a0c7dd123bf00c552b8568f7ebadd
David W. Moore <dmoor231@students.kennesaw.edu>
2024-09-30 13:57:43 -0400

    much setup.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ cp .zshrc ~/
 sudo cp config.toml /etc/greetd/config.toml
 cd dwm
 sudo make clean install
-cd .
-cd davidstatus
+cd ~/david-dwm/davidstatus
 sudo make clean install
 mkdir ~/Downloads
-cp background.png ~/Downloads/background.png
+cp .background.png ~/Downloads/background.png
+sudo chsh -s $(which zsh)
+sudo ln -s /var/service/greetd /etc/sv
