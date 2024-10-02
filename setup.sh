7277c9aa5a51acdad86698e30d8bc428d128597c
David W. Moore <dmoor231@students.kennesaw.edu>
2024-10-02 14:07:04 -0400

    setup stuff

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ cd ~/enterdavidsvoid/davidstatus
 sudo make clean install
 cd ~/enterdavidsvoid/slock/
 sudo make clean install
+cp .p10k.zsh ~/
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/jeffreytse/zsh-vi-mode \$ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 mkdir ~/Downloads
 cp background.png ~/Downloads/background.png
 sudo chsh -s $(which zsh)
