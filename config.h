4cedd1001340e843c1ce50411e97a3c8c33a9233
David W. Moore <dmoor231@students.kennesaw.edu>
2024-10-04 02:57:05 -0400

    weupinthisbiii

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -27,11 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,9 +59,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *rofi[] = {"rofi", "-show", "drun", NULL};
 static const char *passwd[] = {"rofi-rbw", NULL};
-static const char *termcmd[] = {"alacritty", NULL};
+static const char *termcmd[] = {"st", NULL};
 static const char *qutebrowser[] = {"qutebrowser", NULL};
 static const char *slock[] = {"slock", NULL};
+static const char *ranger[] = {"ranger", NULL};
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_space, spawn, {.v = rofi}},
@@ -72,6 +70,7 @@ static const Key keys[] = {
     {MODKEY | Mod1Mask, XK_space, spawn, {.v = qutebrowser}},
     {ControlMask, XK_Tab, spawn, {.v = passwd}},
     {ControlMask | Mod1Mask, XK_Delete, spawn, {.v = slock}},
+    {MODKEY | Mod1Mask, XK_c, spawn, {.v = ranger}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
--- dwm/config.h
+++ dwm/config.h
@@ -57,11 +57,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofi[] = {"rofi", "-show", "drun", NULL};
 static const char *passwd[] = {"rofi-rbw", NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *qutebrowser[] = {"qutebrowser", NULL};
 static const char *slock[] = {"slock", NULL};
+static const char *ranger[] = {"st","ranger", NULL};
+static const char *network[] = {"st", "nmtui", NULL};
+static const char *bitwarden[] = {"st", "rofi-rbw", NULL};
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_space, spawn, {.v = rofi}},
@@ -69,6 +73,9 @@ static const Key keys[] = {
     {MODKEY | Mod1Mask, XK_space, spawn, {.v = qutebrowser}},
     {ControlMask, XK_Tab, spawn, {.v = passwd}},
     {ControlMask | Mod1Mask, XK_Delete, spawn, {.v = slock}},
+    {MODKEY | Mod1Mask, XK_c, spawn, {.v = ranger}},
+    {MODKEY | ControlMask, XK_x, spawn, {.v = network}},
+    {MODKEY | ControlMask, XK_z, spawn, {.v = bitwarden}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
