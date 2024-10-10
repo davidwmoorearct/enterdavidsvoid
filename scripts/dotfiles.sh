2ff1c224f1acb6c0c735836577857c8bd194e1b2
David W. Moore <dmoor231@students.kennesaw.edu>
2024-10-09 21:21:19 -0400

    oh my, gord lord, tell em drinks on me

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,6 +1,6 @@
 ln -s ~/enterdavidsvoid/dotfiles/.xinitrc ~/.xinitrc
 ln -s ~/enterdavidsvoid/dotfiles/.zshrc ~/.zshrc
-ln -s ~/enterdavidsvoid/dotfiles/.fonts ~/.fonts
+ln -s ~/enterdavidsvoid/dotfiles/.fonts ~/
 ln -s ~/enterdavidsvoid/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/enterdavidsvoid/suckless/chadwm ~/.config/chadwm
 sudo ln -s ~/enterdavidsvoid/config/config.toml /etc/greetd/config.toml
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-sh ~/enterdavidsvoid/scripts/installer.sh
-sh ~/enterdavidsvoid/scripts/dotfiles.sh
-sh ~/enterdavidsvoid/scripts/suckless.sh
-sh ~/enterdavidsvoid/scripts/zplugins.sh
-sh ~/enterdavidsvoid/scripts/admin.sh
+sh ~/"enterdavidsvoid/scripts/installer.sh"
+sh ~/"enterdavidsvoid/scripts/dotfiles.sh"
+sh ~/"enterdavidsvoid/scripts/suckless.sh"
+sh ~/"enterdavidsvoid/scripts/zplugins.sh"
+sh ~/"enterdavidsvoid/scripts/admin.sh"
 
