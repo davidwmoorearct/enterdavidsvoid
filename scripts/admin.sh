dbe3bbe41d4ce377b0c60adedb8b86cb4d6c16c9
David W. Moore <dmoor231@students.kennesaw.edu>
2024-11-30 12:49:58 -0500

    Updated config?

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -3,6 +3,10 @@ mkdir ~/Downloads
 ln -s ~/enterdavidsvoid/config/background.png ~/Downloads/background.png
 fc-cache -fv
 sudo chsh -s $(which zsh)
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+flatpak remote-add --system elementary https://flatpak.elementary.io/repo.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo rm /var/service/wpa_supplicant
 sudo ln -s /etc/sv/dbus/ /var/service
 sudo ln -s /etc/sv/NetworkManager/ /var/service
 sudo ln -s /etc/sv/greetd/ /var/service
